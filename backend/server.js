3bd9f46c12602656f6047027ce4fcf774e688846
Varun Sharma <varunshx@gmail.com>
2024-10-15 11:47:15 +0530

    write some code for deploy

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,13 @@ import educationRoute from "./routes/education.route.js";
 import skillsRoute from "./routes/skills.route.js";
 import experienceRoute from "./routes/experience.route.js";
 import linksRoute from "./routes/links.route.js";
+import path from 'path';
+
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json());
 
@@ -19,8 +23,11 @@ app.use(express.json());
 app.use('/api/links', linksRoute);
 // app.use('/api/experience', experienceRoute);
 app.use('/api/portfolio', getData);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'html.html'));
+})
 
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     connectDB();
